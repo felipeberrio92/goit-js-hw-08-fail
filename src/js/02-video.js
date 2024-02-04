40e710f89bb62a210903b69bdc1b3083f5085f4b
felipeberrio92 <felipeberriomarin92@gmail.com>
2024-02-04 02:22:29 -0500

    se realiza tarea 1 y 2 del modulo 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.getElementById('vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+const saveCurrentTime = throttle(function (time) {
+    localStorage.setItem('videoplayer-current-time', time);
+  }, 1000);
+  
+  player.on('timeupdate', function (data) {
+    const currentTime = data.seconds;
+    saveCurrentTime(currentTime);
+  });
+
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
 
